d72e4ad4bfa6254e0eae1b5ae15d86c4950733f3
Andy Brett <andy@andybrett.com>
2013-10-21 21:11:45 -0700

    show spinner

--- app/assets/javascripts/urlminder.js
+++ app/assets/javascripts/urlminder.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
+  function showSpinner() {
+    var spinnerOpts = {
+      lines: 13, // The number of lines to draw
+      length: 20, // The length of each line
+      width: 10, // The line thickness
+      radius: 30, // The radius of the inner circle
+      corners: 1, // Corner roundness (0..1)
+      rotate: 0, // The rotation offset
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: '#000', // #rgb or #rrggbb or array of colors
+      speed: 1, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: false, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: 'auto', // Top position relative to parent in px
+      left: 'auto' // Left position relative to parent in px
+    };
+    var target = document.getElementById('spinner');
+    var spinner = new Spinner(opts).spin(target);
+  }
+
   $(".add-url").click(function() {
     var lastURLId = $("#new_goal input.url:last").attr("id")
     var index = lastURLId.slice(lastURLId.length - 1) - 0;
@@ -27,6 +50,7 @@ $(document).ready(function() {
 
   $("form#new_goal").submit(function() {
     if (validateForm()) {
+      showSpinner();
       $(this).find("button").attr("disabled", "disabled");
       $(this).submit();
     }
@@ -41,25 +65,6 @@ $(document).ready(function() {
 
   $(".refresh-goal").click(function() {
     $(this).attr("disabled", "disabled");
-    var opts = {
-      lines: 13, // The number of lines to draw
-      length: 20, // The length of each line
-      width: 10, // The line thickness
-      radius: 30, // The radius of the inner circle
-      corners: 1, // Corner roundness (0..1)
-      rotate: 0, // The rotation offset
-      direction: 1, // 1: clockwise, -1: counterclockwise
-      color: '#000', // #rgb or #rrggbb or array of colors
-      speed: 1, // Rounds per second
-      trail: 60, // Afterglow percentage
-      shadow: false, // Whether to render a shadow
-      hwaccel: false, // Whether to use hardware acceleration
-      className: 'spinner', // The CSS class to assign to the spinner
-      zIndex: 2e9, // The z-index (defaults to 2000000000)
-      top: 'auto', // Top position relative to parent in px
-      left: 'auto' // Left position relative to parent in px
-    };
-    var target = document.getElementById('spinner');
-    var spinner = new Spinner(opts).spin(target);
+    showSpinner();
   });
 });
