8d7d8908eead2247b9d594fac2d2877cf825f2e4
Andy Brett <andy@andybrett.com>
2013-10-21 21:04:29 -0700

    error handling, add flash notices

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -7,19 +7,23 @@ class GoalsController < ApplicationController
     goal.rate = params[:goal][:rate]
     goal.slug = params[:goal][:slug]
     goal.title = params[:goal][:title]
+    goal.refreshed = Time.now
     params[:goal].select { |k, v| k.match(/^url/) }.each { |k, v| goal.urls << v }
-    goal.save
-
-    bee.create_goal({
-      :title => goal.title,
-      :slug  => goal.slug,
-      :rate  => goal.rate,
-      :goal_type => "biker",
-      :initval => goal.count_words, 
-      :goaldate => (Time.now + 1.year).to_i
-      })
-
-    redirect_to root_url
+    
+    begin
+      bee.create_goal({
+        :title => goal.title,
+        :slug  => goal.slug,
+        :rate  => goal.rate,
+        :goal_type => "biker",
+        :initval => goal.count_words, 
+        :goaldate => (Time.now + 1.year).to_i
+        })
+      goal.save  
+      redirect_to root_url, :flash => { :notice => "Beeminder goal created" }
+    rescue Exception => e
+      redirect_to root_url, :flash => { :error => "Could not create that goal on Beeminder" }
+    end
   end
 
   def update
